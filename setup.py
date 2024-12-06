f08700f798d8429d6fbea57815e0cca66ccc1c3b
Chetan <chetanfern@gmail.com>
2024-12-06 19:31:56 +0530

    push code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="Wafer",
+    version="0.0.1",
+    author="Chetan",
+    author_email="chetanfern@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
