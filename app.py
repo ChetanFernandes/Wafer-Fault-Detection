aabe2ee4fb8817664e94444fb4b2b05ae714a288
Chetan <chetanfern@gmail.com>
2024-12-06 18:33:01 +0530

    run workflow

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def upload():
 
     except Exception as e:
         raise Exception(e,sys)
-    
+
 #Execution will start here now
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug= True)
